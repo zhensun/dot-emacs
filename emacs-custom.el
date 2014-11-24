9071f7bfa1e119028c9fee345a7119eab85f28d7
Zhen Sun <zsun1@hfhs.org>
2014-11-24 13:46:03 -0500

    Set init buffer to *scratch*

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-buffer-choice t)
  '(org-src-fontify-natively t)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
